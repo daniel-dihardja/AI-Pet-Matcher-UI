7681d1918fe8ab726946ac3315a8524e88faa9df
Daniel Dihardja <daniel@dihardja.de>
2024-07-09 17:48:54 +0200

    fix response json parse

--- app/routes/api.status.tsx
+++ app/routes/api.status.tsx
@@ -4,9 +4,7 @@ import appCache from "~/utils/app-cache.server";
 export const loader: LoaderFunction = async () => {
   const data = appCache.get("matches") as string;
   if (data) {
-    const body = JSON.parse(data.body);
-    const matches = JSON.parse(body.response.content);
-    console.log("data: ", data);
+    const matches = JSON.parse(data.response.content);
     return json({ matches });
   }
   return json({ matches: null });

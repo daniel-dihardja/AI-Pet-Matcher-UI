92306f524304e4e2be37b3d79286f9c60d5aa9cd
Daniel Dihardja <daniel@dihardja.de>
2024-07-13 19:52:36 +0200

    fix handle response in local and prod environment

--- app/routes/api.status.tsx
+++ app/routes/api.status.tsx
@@ -4,7 +4,10 @@ import appCache from "~/utils/app-cache.server";
 export const loader: LoaderFunction = async () => {
   const data = appCache.get("matches") as string;
   if (data) {
-    const matches = JSON.parse(data.response.content);
+    const { GET_MATCHING_PETS_API_URL } = process.env;
+    const isLocalhost = GET_MATCHING_PETS_API_URL?.includes("localhost");
+    const body = isLocalhost ? JSON.parse(data.body) : data;
+    const matches = JSON.parse(body.response.content);
     return json({ matches });
   }
   return json({ matches: null });
